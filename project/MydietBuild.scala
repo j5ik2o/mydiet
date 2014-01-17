35180eee59d5e95abce788cf798e8f203c417668
Junichi Kato <junichi.kato@gree.net>
2014-01-17 09:43:30 +0900

    refactor

--- project/MydietBuild.scala
+++ project/MydietBuild.scala
@@ -3,15 +3,41 @@ import sbt.Keys._
 
 object MydietBuild extends Build {
 
+  val commonSettings = Project.defaultSettings ++ 
+    net.virtualvoid.sbt.graph.Plugin.graphSettings ++ Seq(
+    organization := "com.github.j5ik2o.mydiet",
+    version := "0.0.1-SNAPSHOT",
+    scalaVersion := "2.10.3",
+    scalacOptions ++= Seq("-encoding", "UTF-8", "-feature", "-deprecation", "-unchecked"),
+    javacOptions ++= Seq("-encoding", "UTF-8", "-deprecation"),
+    resolvers ++= Seq(
+      "Twitter Repository" at "http://maven.twttr.com/",
+      "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
+      "Sonatype Release Repository" at "https://oss.sonatype.org/content/repositories/releases/",
+      "Sonatype Snapshot Repository" at "https://oss.sonatype.org/content/repositories/snapshots/",
+      "Seasar Repository" at "http://maven.seasar.org/maven2/"
+    ),
+    libraryDependencies ++= Seq(
+      "org.scala-lang" % "scala-reflect" % "2.10.3",
+      "junit" % "junit" % "4.8.1" % "test",
+      "org.hamcrest" % "hamcrest-all" % "1.3" % "test",
+      "org.mockito" % "mockito-core" % "1.9.5" % "test",
+      "org.specs2" %% "specs2" % "2.0" % "test",
+      "org.seasar.util" % "s2util" % "0.0.1"
+    ),
+    fork in Test := true,
+    publishMavenStyle := true,
+    publishArtifact in Test := false,
+    pomIncludeRepository := {
+      _ => false
+    }
+  )
+
   lazy val mydiet = Project(
     id = "mydiet",
     base = file("."),
-    settings = Project.defaultSettings ++ Seq(
-      name := "mydiet",
-      organization := "com.github.j5ik2o.mydiet",
-      version := "0.1-SNAPSHOT",
-      scalaVersion := "2.10.3"
-      // add other settings here
+    settings = commonSettings ++ Seq(
+      name := "mydiet"
     )
   )
 }
