4f553e89285b38b4b5df617fe0fe9b99aa83aa4a
Junichi Kato <junichi.kato@gree.net>
2014-01-30 18:32:50 +0900

    temp commit

--- src/main/scala/com/github/j5ik2o/mydiet/domain/user/UserRepository.scala
+++ src/main/scala/com/github/j5ik2o/mydiet/domain/user/UserRepository.scala
@@ -1,16 +1,37 @@
 package com.github.j5ik2o.mydiet.domain.user
 
 import org.sisioh.dddbase.core.lifecycle.Repository
+import org.sisioh.dddbase.core.lifecycle.memory.mutable.async.AbstractAsyncRepositoryOnMemory
 import scala.concurrent.Future
 import scala.util.Try
+import org.sisioh.dddbase.core.lifecycle.async.AsyncRepository
+import org.sisioh.dddbase.core.lifecycle.sync.SyncRepository
+import org.sisioh.dddbase.core.lifecycle.memory.mutable.sync.AbstractSyncRepositoryOnMemory
 
-trait UserRepository extends Repository[UserId, User, Try] {
+trait UserRepository[M[+ _]] extends Repository[UserId, User, M] {
 
 }
 
+trait AsyncUserRepository extends AsyncRepository with UserRepository[Future]
+
+trait SyncUserRepository extends SyncRepository with UserRepository[Try]
+
+class AsyncUserRepositoryOnMemoryImpl
+  extends AbstractAsyncRepositoryOnMemory[UserId, User]() with AsyncUserRepository {
+
+  type This = AsyncUserRepositoryOnMemoryImpl
+
+}
+
+class SyncUserRepositoryOnMemoryImpl
+  extends AbstractSyncRepositoryOnMemory[UserId, User]() with SyncUserRepository {
+
+  type This = AsyncUserRepositoryOnMemoryImpl
+
+}
 
 object UserRepository {
 
-    def ofMemory: UserRepository = new UserRepositoryOnMemory
+  def ofMemory: UserRepository = new UserRepositoryOnMemory
 
 }
