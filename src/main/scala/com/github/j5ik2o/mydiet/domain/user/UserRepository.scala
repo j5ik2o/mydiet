3cfe7c075e0e75478bc234420e4d3c4a97debd05
Junichi Kato <junichi.kato@gree.net>
2014-02-07 01:01:36 +0900

    refactor

--- src/main/scala/com/github/j5ik2o/mydiet/domain/user/UserRepository.scala
+++ src/main/scala/com/github/j5ik2o/mydiet/domain/user/UserRepository.scala
@@ -1,27 +1,18 @@
 package com.github.j5ik2o.mydiet.domain.user
 
-<<<<<<< HEAD
 import scala.language.higherKinds
 import org.sisioh.dddbase.core.lifecycle.Repository
 import org.sisioh.dddbase.core.lifecycle.memory.mutable.async.AbstractAsyncRepositoryOnMemory
 import scala.concurrent.Future
 import org.sisioh.dddbase.core.lifecycle.async.AsyncRepository
 
-trait UserRepository[M[+ _]] extends Repository[UserId, User, M] {
-=======
-import org.sisioh.dddbase.core.lifecycle.async.AsyncRepository
-
-trait UserRepository extends AsyncRepository[UserId, User] {
->>>>>>> master
-
-}
+trait UserRepository[M[+ _]] extends Repository[UserId, User, M]
 
 trait AsyncUserRepository extends AsyncRepository[UserId, User] with UserRepository[Future]
 
 class AsyncUserRepositoryOnMemoryImpl
   extends AbstractAsyncRepositoryOnMemory[UserId, User]() with AsyncUserRepository {
 
-<<<<<<< HEAD
   type This = AsyncUserRepositoryOnMemoryImpl
 
 }
@@ -29,8 +20,5 @@ class AsyncUserRepositoryOnMemoryImpl
 object AsyncUserRepository {
 
   def ofMemory: AsyncUserRepository = new AsyncUserRepositoryOnMemoryImpl
-=======
-  def ofMemory: UserRepository = new UserRepositoryOnMemory
->>>>>>> master
 
 }
